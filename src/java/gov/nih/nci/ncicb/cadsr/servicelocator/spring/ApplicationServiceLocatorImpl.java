29c6e3b61d45b48b288f3cb379732f7b611e1bd4
jiangj <jiangj>
2005-11-29 15:35:00 +0000

    initial version
    SVN-Revision: 13

---
+++ src/java/gov/nih/nci/ncicb/cadsr/servicelocator/spring/ApplicationServiceLocatorImpl.java
@@ -0,0 +1,72 @@
+package gov.nih.nci.ncicb.cadsr.servicelocator.spring;
+import gov.nih.nci.ncicb.cadsr.cdebrowser.service.CDEBrowserService;
+import gov.nih.nci.ncicb.cadsr.cdebrowser.tree.service.CDEBrowserTreeService;
+import gov.nih.nci.ncicb.cadsr.servicelocator.ApplicationServiceLocator;
+import gov.nih.nci.ncicb.cadsr.servicelocator.ServiceLocatorException;
+import gov.nih.nci.ncicb.cadsr.spring.ApplicationContextFactory;
+import gov.nih.nci.ncicb.cadsr.ocbrowser.service.OCBrowserService;
+
+public class ApplicationServiceLocatorImpl implements ApplicationServiceLocator
+{
+
+ private  OCBrowserService ocBrowserService = null;
+
+ private  CDEBrowserTreeService treeService = null;
+
+ private  CDEBrowserService cdebrowserService = null;
+
+  public ApplicationServiceLocatorImpl()
+  {
+  }
+
+   public OCBrowserService findOCBrowserService() throws ServiceLocatorException
+   {
+     if(ocBrowserService==null)
+     {
+          try
+          {
+            Object obj = new SpringObjectLocatorImpl().findObject("OCBrowserService");
+            ocBrowserService = (OCBrowserService) (obj);
+          }
+          catch (Exception e)
+          {
+            throw new ServiceLocatorException("Exp while locating oc serice",e);
+          }
+     }
+     return ocBrowserService;
+   }
+   public CDEBrowserTreeService findTreeService() throws ServiceLocatorException
+   {
+     if(treeService==null)
+     {
+          try
+          {
+            Object obj = new SpringObjectLocatorImpl().findObject("treeService");
+            treeService = (CDEBrowserTreeService) (obj);
+          }
+          catch (Exception e)
+          {
+            throw new ServiceLocatorException("Exp while locating tree serice",e);
+          }
+     }
+     return treeService;
+   }
+
+   public CDEBrowserService findCDEBrowserService() throws ServiceLocatorException
+   {
+     if(cdebrowserService==null)
+     {
+          try
+          {
+            Object obj = new SpringObjectLocatorImpl().findObject("CDEBrowserService");
+            cdebrowserService = (CDEBrowserService) (obj);
+          }
+          catch (Exception e)
+          {
+            throw new ServiceLocatorException("Exp while locating cdebrowserService service",e);
+          }
+     }
+     return cdebrowserService;
+   }
+
+}
