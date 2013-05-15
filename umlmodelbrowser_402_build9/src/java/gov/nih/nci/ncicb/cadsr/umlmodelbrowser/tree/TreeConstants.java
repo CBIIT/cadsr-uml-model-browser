3a63ae19848b16d0b40239eefe92fe21fb228e0e
Harika Jonnala <jonnalah@mail.nih.gov>
2013-05-15 16:56:41 +0000

    .
    git-svn-id: https://ncisvn.nci.nih.gov/svn/umlmodelbrowser/trunk@420 bad2af84-28b6-4caa-935a-a93a784cf199

--- umlmodelbrowser_402_build9/src/java/gov/nih/nci/ncicb/cadsr/umlmodelbrowser/tree/TreeConstants.java
+++
@@ -1,8 +0,0 @@
-package gov.nih.nci.ncicb.cadsr.umlmodelbrowser.tree;
-
-public interface TreeConstants  {
-  
-  public static final String TREE_BREADCRUMBS="treeBreadCrumbs";
-  
-  public static final String BR_CONTEXT_EXCLUDE_LIST_STR = "brContextExcludeListStr";
-}
